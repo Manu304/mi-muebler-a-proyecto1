abaf0297c367abcde21b4295247c6064096c69ce
Manuel Rojas <manuerjs70@gmail.com>
2021-09-02 12:35:50 -0600

    Mofique nombre de variables en los metodos

--- src/main/java/com/mrojas/pruebas/mi_muebleria/repositories/users/UserRepository.java
+++ src/main/java/com/mrojas/pruebas/mi_muebleria/repositories/users/UserRepository.java
@@ -13,7 +13,7 @@ import java.util.List;
  */
 public class UserRepository implements Repository<Usuario> {
 
-    //private Connection connection;
+    // private Connection connection;
     private static final String TABLA = "usuario";
 
     public UserRepository() {
@@ -31,30 +31,14 @@ public class UserRepository implements Repository<Usuario> {
         Usuario user = null;
         String sql = "SELECT * FROM " + TABLA + " WHERE nombre = ? AND password = ?";
 
-        try ( Connection connection = getConnection();  PreparedStatement stmt = connection.prepareStatement(sql)) {
+        try (Connection connection = getConnection(); PreparedStatement stmt = connection.prepareStatement(sql)) {
             stmt.setString(1, nombre);
             stmt.setString(2, password);
-            try ( ResultSet rs = stmt.executeQuery()) {
-                if (rs.next()) {
-                    System.out.println("HE ENCONTRADO USUARIO EN LA BASE");
-                    user = getUsuario(rs);
-                }
-            }
-        }
-        return user;
-    }
-
-    @Override
-    public Usuario porIdentificador(String primaryKey) throws SQLException {
-        Usuario user = null;
-        try ( Connection connection = getConnection();  PreparedStatement stmt = connection.prepareStatement("SELECT * FROM " + TABLA + " WHERE nombre = ?")) {
-            stmt.setString(1, primaryKey);
-            try ( ResultSet rs = stmt.executeQuery()) {
+            try (ResultSet rs = stmt.executeQuery()) {
                 if (rs.next()) {
                     user = getUsuario(rs);
                 }
             }
-
         }
         return user;
     }
@@ -62,7 +46,9 @@ public class UserRepository implements Repository<Usuario> {
     @Override
     public List<Usuario> listar() throws SQLException {
         List<Usuario> usuarios = new ArrayList<>();
-        try ( Connection connection = getConnection();  Statement stmt = connection.createStatement();  ResultSet rs = stmt.executeQuery("SELECT * FROM " + TABLA)) {
+        try (Connection connection = getConnection();
+                Statement stmt = connection.createStatement();
+                ResultSet rs = stmt.executeQuery("SELECT * FROM " + TABLA)) {
             while (rs.next()) {
                 Usuario user = getUsuario(rs);
                 usuarios.add(user);
@@ -72,38 +58,19 @@ public class UserRepository implements Repository<Usuario> {
     }
 
     @Override
-    public void guardar(Usuario t) throws SQLException {
-        String sql;
-        boolean existe = t.getNombre() != null && porIdentificador(t.getNombre()) != null;
-        if (existe) {
-            sql = "UPDATE " + TABLA + " SET password = ?, tipo = ?, activo = ? WHERE nombre = ?";
-            System.out.println("estoy en el repository update usuario");
-            System.out.println("ingrese actividad1: " + t.isActivo());
-        }else{
-            sql  = "INSERT INTO " + TABLA + " (password, tipo, activo, nombre) VALUES (?, ?, ?, ?)";
-            System.out.println("estoy en el repository guardar usuario");
-        }
-
-        try ( Connection connection = getConnection();  PreparedStatement stmt = connection.prepareStatement(sql)) {
-            stmt.setString(1, t.getPassword());
-            stmt.setInt(2, t.getTipo());
-            stmt.setBoolean(3, t.isActivo());
-            System.out.println("ingrese actividad: " + t.isActivo());
-            stmt.setString(4, t.getNombre());
-            
-            stmt.executeUpdate();
-        }
-    }
-
-    @Override
-    public void eliminar(String primaryKey) throws SQLException {
-        String sql = "DELETE FROM " + TABLA + " WHERE nombre = ?";
-        try ( Connection connection = getConnection();  PreparedStatement stmt = connection.prepareStatement(sql)) {
-            System.out.println("estoy en el repository eliminar usuario");
+    public Usuario porIdentificador(String primaryKey) throws SQLException {
+        Usuario user = null;
+        try (Connection connection = getConnection();
+                PreparedStatement stmt = connection.prepareStatement("SELECT * FROM " + TABLA + " WHERE nombre = ?")) {
             stmt.setString(1, primaryKey);
-            stmt.executeUpdate();
-        }
+            try (ResultSet rs = stmt.executeQuery()) {
+                if (rs.next()) {
+                    user = getUsuario(rs);
+                }
+            }
 
+        }
+        return user;
     }
 
     @Override
@@ -112,7 +79,37 @@ public class UserRepository implements Repository<Usuario> {
     }
 
     @Override
-    public void eliminar(Integer intgr) throws SQLException {
+    public void guardar(Usuario t) throws SQLException {
+        String sql;
+        boolean existe = t.getNombre() != null && porIdentificador(t.getNombre()) != null;
+        if (existe) {
+            sql = "UPDATE " + TABLA + " SET password = ?, tipo = ?, activo = ? WHERE nombre = ?";
+        } else {
+            sql = "INSERT INTO " + TABLA + " (password, tipo, activo, nombre) VALUES (?, ?, ?, ?)";
+        }
+
+        try (Connection connection = getConnection(); PreparedStatement stmt = connection.prepareStatement(sql)) {
+            stmt.setString(1, t.getPassword());
+            stmt.setInt(2, t.getTipo());
+            stmt.setBoolean(3, t.isActivo());
+            stmt.setString(4, t.getNombre());
+
+            stmt.executeUpdate();
+        }
+    }
+
+    @Override
+    public void eliminar(String primaryKey) throws SQLException {
+        String sql = "DELETE FROM " + TABLA + " WHERE nombre = ?";
+        try (Connection connection = getConnection(); PreparedStatement stmt = connection.prepareStatement(sql)) {
+            stmt.setString(1, primaryKey);
+            stmt.executeUpdate();
+        }
+
+    }
+
+    @Override
+    public void eliminar(Integer primaryKey) throws SQLException {
 
     }
 
