792806742de054ec9adc8976885df5f91e090a86
Manuel Rojas <manuerjs70@gmail.com>
2021-09-06 11:56:11 -0600

    Modifique metodos y agregue documentacion

--- src/main/java/com/mrojas/pruebas/mi_muebleria/util/ConexionBaseDatos.java
+++ src/main/java/com/mrojas/pruebas/mi_muebleria/util/ConexionBaseDatos.java
@@ -14,17 +14,26 @@ import java.sql.*;
 public class ConexionBaseDatos {
 
     private static final String URL = "jdbc:mysql://localhost:3306/mi_muebleria";
-    private static final String USERNAME = "root2";
+    private static final String USERNAME = "mrojas";
     private static final String PASSWORD = "1234";
 
     /**
      * Metodo para crear una conexion hacia la base de datos
      * @return Retorna una conexion para utilizar la base de datos
+     * @throws ClassNotFoundException
      * @throws SQLException
      */
 
     public static Connection getConnection() throws SQLException {
-        return DriverManager.getConnection(URL, USERNAME, PASSWORD);
+        
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+           return DriverManager.getConnection(URL, USERNAME, PASSWORD); 
+        } catch (ClassNotFoundException e) {
+            System.out.println("no se ha encotrado el driver de conexion");
+            return null;
+        }
+        
     }
 
 }
--- src/main/java/com/mrojas/pruebas/mi_muebleria/util/DataValidator.java
+++ src/main/java/com/mrojas/pruebas/mi_muebleria/util/DataValidator.java
@@ -2,6 +2,14 @@ package com.mrojas.pruebas.mi_muebleria.util;
 
 public class DataValidator {
 
+    /**
+     * Metodo que valida un string como entero positivo mayor a cero
+     * 
+     * @param valor El String a validar
+     * @return Retorna true si es entero mayor a cero, de lo contrario retorna
+     *         false.
+     */
+
     public static boolean isEntero(String valor) {
         boolean valido = false;
 
@@ -17,22 +25,38 @@ public class DataValidator {
         return valido;
     }
 
-    public static boolean isDouble(String valor){
-        boolean valido = false;
+    /**
+     * Metodo que valida un string como double positivo mayor a cero
+     * 
+     * @param valor El String a validar
+     * @return Retorna true si es un double mayor a cero, de lo contrario retorna
+     *         false
+     */
 
-        try {
-            double cantidad = Double.parseDouble(valor);
-            if (cantidad > 0) {
-                valido = true;
+    public static boolean isDouble(String valor) {
+        boolean valido = false;
+        if (!valor.endsWith(".")) {
+            try {
+                double cantidad = Double.parseDouble(valor);
+                if (cantidad > 0) {
+                    valido = true;
+                }
+            } catch (NullPointerException | NumberFormatException e) {
+                System.out.println("No es un caracter double");
             }
-        } catch (NullPointerException | NumberFormatException e) {
-            System.out.println("No es un caracter double");
         }
 
         return valido;
     }
 
-    public static boolean isValidString(String valor){
+    /**
+     * Metodo que valida un string como no vacío
+     * 
+     * @param valor El String a validar
+     * @return Retorna true si el string no es vacio o null.
+     */
+
+    public static boolean isValidString(String valor) {
         boolean valido = true;
         if (valor == null || valor.isBlank()) {
             valido = false;
@@ -40,19 +64,37 @@ public class DataValidator {
         return valido;
     }
 
-    public static boolean isAlfanumerico(String valor){
+    /**
+     * Metodo que valida un String como alfanumerico
+     * 
+     * @param valor El String a validar
+     * @return Retorna true si el string contiene solo letras del abecedario inglés,
+     *         espacios y números, de lo contrario retorna false.
+     */
+
+    public static boolean isAlfanumerico(String valor) {
         boolean valido = true;
         for (int i = 0; i < valor.length(); i++) {
-            if (!Character.isAlphabetic(valor.charAt(i)) && valor.charAt(i) != ' ') {
+            char char1 = valor.toLowerCase().charAt(i);
+
+            if (!((char1 >= 'a' && char1 <= 'z') || (char1 >= '0' && char1 <= '9')) && char1 != ' ') {
                 valido = false;
             }
         }
         return valido;
     }
 
-    public static double redondearNumero(double numero, int decimales){
+    /**
+     * Método para redondear un double a dos decimales significativos.
+     * 
+     * @param numero    El número que se desea aproximar
+     * @param decimales La cantidad de decimales que se desan en el número
+     * @return Devuelve un número con la cantidad de decimales indicada.
+     */
+
+    public static double redondearNumero(double numero, int decimales) {
         double cantidadCero = Math.pow(10.0, decimales);
-        double valor = Math.round(numero*cantidadCero)/cantidadCero;
+        double valor = Math.round(numero * cantidadCero) / cantidadCero;
         return valor;
     }
 
